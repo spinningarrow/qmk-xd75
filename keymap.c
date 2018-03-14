7246e81c17ebf982e87c4fdd76b49c3f2ac66036
Sahil Bajaj <spinningarrow@users.noreply.github.com>
2018-03-14 23:00:31 +0800

    Swap Caps Lock for Control
    Sometimes it's nice to have Control in the bottom row.

--- keymap.c
+++ keymap.c
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   { KC_TAB,        KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,   KC_Y,    KC_U,   KC_I,    KC_O,    KC_P,    KC_LBRC,    KC_RBRC, KC_BSLS, KC_INS },
   { CTL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,   KC_G,   KC_H,    KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT,    KC_ENT,  KC_ENT,  KC_HOME },
   { KC_LSFT,       KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,   KC_N,    KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,    KC_RSFT, KC_UP,   KC_END },
-  { TT(F_SCROLL),  KC_CAPS, KC_LALT, KC_LGUI, KC_SPC, KC_SPC, KC_BSPC, KC_SPC, KC_SPC,  KC_RGUI, KC_RALT, TT(F_KEYS), KC_LEFT, KC_DOWN, KC_RGHT },
+  { TT(F_SCROLL),  KC_LCTL, KC_LALT, KC_LGUI, KC_SPC, KC_SPC, KC_BSPC, KC_SPC, KC_SPC,  KC_RGUI, KC_RALT, TT(F_KEYS), KC_LEFT, KC_DOWN, KC_RGHT },
  },
 
  [F_KEYS] = {
